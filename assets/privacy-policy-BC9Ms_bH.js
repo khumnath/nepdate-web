be2f8052600adde8fa1fbf463d44a00832bebffe
khumnath cg <nath.khum@gmail.com>
2026-04-08 08:59:02 +0545

    rashifal share card resize, tab menu close on back

---
+++ assets/privacy-policy-BC9Ms_bH.js
@@ -0,0 +1,1 @@
+import{n as t,R as i}from"./index-DwU_dvBl.js";import{o as e,j as o}from"./vendor-react-BymtWz9D.js";import n from"./PrivacyPage-GHlxdbON.js";import"./vendor-other-CsJX9OmL.js";import"./vendor-leaflet-Bjm2FGmM.js";const r=document.getElementById("privacy-root");r&&e.createRoot(r).render(o.jsx(t,{children:o.jsx(i,{children:o.jsx(n,{onBack:()=>{window.location.href="/"}})})}));
--- assets/privacy-policy-D7x7_wqN.js
+++
@@ -1,1 +0,0 @@
-import{n as t,R as i}from"./index-B0H6g88d.js";import{o as e,j as o}from"./vendor-react-BymtWz9D.js";import n from"./PrivacyPage-B0ALGtUF.js";import"./vendor-other-CsJX9OmL.js";import"./vendor-leaflet-Bjm2FGmM.js";const r=document.getElementById("privacy-root");r&&e.createRoot(r).render(o.jsx(t,{children:o.jsx(i,{children:o.jsx(n,{onBack:()=>{window.location.href="/"}})})}));
